7bfc3f340a061b83bec735c1fa97a15df04eccfc
Tim Paisley <tepaisley@gmail.com>
2023-03-03 14:15:13 +1300

    Update footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
-    <footer className="border-t p-4 mt-16 text-center text-gray-500 text-sm">
-      2023, Tim Paisley
+    <footer className="max-w-5xl m-auto border-t p-4 mt-16 text-center text-gray-500 text-sm">
+      Website by Tim Paisley
     </footer>
   );
 }
