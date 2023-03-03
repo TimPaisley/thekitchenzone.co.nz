305a7539ed65921ecc007aba59f20b1215ef55ba
Tim Paisley <tepaisley@gmail.com>
2023-03-03 14:18:43 +1300

    Add gradient to hero text

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -9,7 +9,7 @@ export default function Hero() {
     >
       <div className="absolute bg-black/75 w-full h-full" />
       <div className="relative py-16 sm:py-28 max-w-3xl m-auto flex flex-col justify-center text-center text-white">
-        <h1 className="text-5xl font-bold mb-4">
+        <h1 className="text-5xl font-bold mb-4 text-transparent bg-clip-text bg-gradient-to-l from-red-400 to-red-700">
           Kitchens, vanities and furniture
         </h1>
 
